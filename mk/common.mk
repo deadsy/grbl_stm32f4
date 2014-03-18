7bfbfdd71d6171daadcfdbcc437af4d77c211d2a
Jason Harris <jth@mibot.com>
2014-03-18 07:44:06 -0700

    Created a top level makefile. Combined object files into libraries.

--- mk/common.mk
+++ mk/common.mk
@@ -2,6 +2,7 @@
 XTOOLS_DIR = /opt/gcc-arm-none-eabi-4_8-2013q4/bin
 X_CC = $(XTOOLS_DIR)/arm-none-eabi-gcc
 X_OBJCOPY = $(XTOOLS_DIR)/arm-none-eabi-objcopy
+X_AR = $(XTOOLS_DIR)/arm-none-eabi-ar
 X_GDB = $(XTOOLS_DIR)/arm-none-eabi-gdb
 
 PATCHFILES = $(sort $(wildcard patches/*.patch))
@@ -21,12 +22,8 @@ CFLAGS += -mfloat-abi=hard -mfpu=fpv4-sp-d16
 .c.o:
 	$(X_CC) $(INCLUDE) $(DEFINES) $(CFLAGS) -c $< -o $@
 
-.PHONY: untar
-untar:
+.PHONY: untar_patch_overlay
+untar_patch_overlay:
 	tar jxf $(TARFILE)
 	$(PATCH_CMD)
-
-
-
-
-
+	cd files; tar -c -f - * | (cd ../$(WORK_DIR) ; tar xfp -)
