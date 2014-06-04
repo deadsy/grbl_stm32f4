98ca8cd4107471bd9dff25d33f952cb706365b1b
Jason Harris <jth@mibot.com>
2014-06-04 07:50:21 -0700

    More USART driver work.

--- board/usart.c
+++ board/usart.c
@@ -10,6 +10,18 @@ USART Driver
 #include "usart.h"
 
 //-----------------------------------------------------------------------------
+
+// enable the peripheral clock for the usart
+static void enable_usart_clock(USART_TypeDef *usart)
+{
+    if (usart == USART1) {
+        RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+    } else if (usart == USART2) {
+        RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+    }
+}
+
+//-----------------------------------------------------------------------------
 // setup a serial port
 // Using USART2/AF7 mapped to PA2(tx) PA3(rx).
 
@@ -36,15 +48,36 @@ uint8_t usart_rx(void)
 void usart_init(void)
 {
     USART_TypeDef* const usart = USART2;
+    uint32_t val;
+
+    // enable peripheral clock
+    enable_usart_clock(usart);
+
+    // disable the uart
+    usart->CR1 &= ~USART_CR1_UE;
+
+    // set CR1
+    val = usart->CR1;
+    usart->CR1= val;
+
+    // set CR2
+    val = usart->CR2;
+    val &= ~(USART_CR2_LINEN | USART_CR2_CLKEN);
+    usart->CR2= val;
+
+    // set CR3
+    val = usart->CR3;
+    val &= ~(USART_CR3_SCEN | USART_CR3_HDSEL | USART_CR3_IREN);
+    usart->CR3= val;
 
     // clear the status register
     usart->SR = 0;
 
     usart->BRR = 0;
-    usart->CR1 = 0;
-    usart->CR2 = 0;
-    usart->CR3 = 0;
     usart->GTPR = 0;
+
+    // enable the uart
+    usart->CR1 |= USART_CR1_UE;
 }
 
 //-----------------------------------------------------------------------------
